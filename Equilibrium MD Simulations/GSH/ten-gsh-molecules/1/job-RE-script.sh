070258d5bec41e1669b97720ed59280d36540333
Saber Boushehri <saberboshehri1371@yahoo.com>
2025-02-08 14:50:43 +0100

    update

---
+++ Equilibrium MD Simulations/GSH/ten-gsh-molecules/1/job-RE-script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p cascade.p
+#SBATCH --gres=gpu:1
+#SBATCH --mincpus=20
+#SBATCH --time=24:00:00
+#SBATCH --job-name="10gsh1"
+
+module use /hits/sw/its/doserbd/cascade/modules/all
+module load GROMACS/2021-fosscuda-2019b
+
+#rerun MD
+
+START=$(date +"%s")
+
+gmx mdrun -deffnm md -cpi md.cpt -append  -dlb yes -ntomp 3 -ntmpi 6 -pme gpu -npme 1 -bonded gpu -nb gpu -maxh 22
+
+END=$(date +"%s")
+
+if [ $(((END-START)/3600)) -lt 2 ]
+then
+  exit
+fi
+
+sbatch job-RE-script.sh
